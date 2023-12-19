1d2bcf91d29a37cdf961487a426a736fd03c0d84
Rafiul007 <rafiul.faisal@gmail.com>
2023-12-19 11:00:46 +0600

    more minions added

--- src/assets/banana.js
+++ src/assets/banana.js
@@ -1,23 +1,46 @@
 import bob from './bob.jpg'
 import carl from './carl.png'
 import darwin from './darwin.jpg'
-
+import kevin from './Kevin.png'
+import stuart from './stuart.png'
+import dave from './dave.png'
 const bananaData = [
-    {
-        img: bob,
-        name: "Bob da Minion",
-        banana: "Bob is a trouble-maker and an attention seeker. He acts naturally like a child, very cute for sure but sometimes, annoying"
-    },
-    {
-        img: carl,
-        name: "Carl da Minion",
-        banana: "Carl is taller with only one eye. He is more cheerful and easy to get along whether with his fellow Minions or with the human"
-    },
-    {
-        img: darwin,
-        name: "Darwin",
-        banana: "Darwin is indeed quite egoistic. His actions cause troubles for Gru and his fellow Minions. He is polite and well-behaved"
-    },
-]
+  {
+    img: kevin,
+    name: "Sir Kevin",
+    banana:
+      "Kevin loves to make fun of and tease people or Minions. He truly cares about the well-being of the Minion tribe ",
+  },
+  {
+    img: bob,
+    name: "Bob da Minion",
+    banana:
+      "Bob is a trouble-maker and an attention seeker. He acts naturally like a child, very cute for sure but sometimes, annoying",
+  },
+  {
+    img: carl,
+    name: "Carl da Minion",
+    banana:
+      "Carl is taller with only one eye. He is more cheerful and easy to get along whether with his fellow Minions or with the human",
+  },
+  {
+    img: darwin,
+    name: "Darwin",
+    banana:
+      "Darwin is indeed quite egoistic. His actions cause troubles for Gru and his fellow Minions. He is polite and well-behaved",
+  },
+  {
+    img: stuart,
+    name: "Stuart",
+    banana:
+      "Stuart is playful, friendly, intelligent and funny. He is skillful at video games like his friend Dave. He is considered to be a slacker.",
+  },
+  {
+    img: dave,
+    name: "Dave",
+    banana:
+      "Dave is an intelligent minion who is kind, caring, and funny, but sometimes accident-prone. He is skillful at video games like his friend Stuart",
+  },
+];
 
 export default bananaData;
